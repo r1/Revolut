e50826af60698da2a0059dec8248620569fc9431
Jack Latimer <jacklatimer03@gmail.com>
2022-02-10 17:47:44 +0000

    done(mobile): finished mobile images

--- components/Main.js
+++ components/Main.js
@@ -21,7 +21,7 @@ export default function Main() {
           </div>
           <p className="text-[#292c2f] text-sm lg:text-xl font-semibold whitespace-pre-wrap py-4 lg:py-6">
             From easy money management, to travel perks and investments.{" "}
-            <br className="hidden lg:block" />
+            <br className="hidden md:block" />
             Open your account in a flash
           </p>
           <a
@@ -45,48 +45,90 @@ export default function Main() {
           </p>
         </div>
       </div>
-      <a href="/" className="lg:px-0 px-4 z-10 hidden lg:block">
+      <a href="/" className="lg:px-0 px-4 z-10 hidden md:block">
         <img
           src="/images/Payments.png"
           alt="Payments"
           className="rounded-2xl"
         />
       </a>
-      <div className="lg:grid grid-cols-2 gap-5 hidden">
-        <a href="/" className="lg:px-0 px-4 z-10">
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Payments_Mobile.png"
+          alt="Payments"
+          className="rounded-2xl"
+        />
+      </a>
+      <div className="md:grid grid-cols-2 gap-0 lg:gap-5 hidden">
+        <a href="/" className="lg:px-0 px-2 z-10">
           <img
             src="/images/SavingsVault.png"
-            alt="Payments"
+            alt="SavingsVault"
             className="rounded-2xl"
           />
         </a>
-        <a href="/" className="lg:px-0 px-4 z-10">
+        <a href="/" className="lg:px-0 px-2 z-10">
           <img
             src="/images/Rewards.png"
-            alt="Payments"
+            alt="Rewards"
             className="rounded-2xl"
           />
         </a>
       </div>
-      <a href="/" className="lg:px-0 px-4 z-10 hidden lg:block">
-        <img src="/images/Cards.png" alt="Payments" className="rounded-2xl" />
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/SavingsVault_Mobile.png"
+          alt="SavingsVault"
+          className="rounded-2xl"
+        />
       </a>
-      <div className="lg:grid grid-cols-2 gap-5 hidden">
-        <a href="/" className="lg:px-0 px-4 z-10">
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Rewards_Mobile.png"
+          alt="Rewards"
+          className="rounded-2xl"
+        />
+      </a>
+      <a href="/" className="lg:px-0 px-4 z-10 hidden md:block">
+        <img src="/images/Cards.png" alt="Cards" className="rounded-2xl" />
+      </a>
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Cards_Mobile.jpeg"
+          alt="Cards"
+          className="rounded-2xl"
+        />
+      </a>
+      <div className="md:grid grid-cols-2 gap-0 lg:gap-5 hidden">
+        <a href="/" className="lg:px-0 px-2 z-10">
           <img
             src="/images/OpenBanking.png"
-            alt="Payments"
+            alt="OpenBanking"
             className="rounded-2xl"
           />
         </a>
-        <a href="/" className="lg:px-0 px-4 z-10">
+        <a href="/" className="lg:px-0 px-2 z-10">
           <img
             src="/images/Subscriptions.png"
-            alt="Payments"
+            alt="Subscriptions"
             className="rounded-2xl"
           />
         </a>
       </div>
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/OpenBanking_Mobile.png"
+          alt="OpenBanking"
+          className="rounded-2xl"
+        />
+      </a>
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Subscriptions_Mobile.png"
+          alt="Subscriptions"
+          className="rounded-2xl"
+        />
+      </a>
       <div className="mx-auto mt-20 lg:mt-44 mb-6 lg:mb-20 z-10 px-10 lg:px-0">
         <div className="text-center">
           <p className="Basier-Regular text-md lg:text-xl font-bold text-[#77828c] mb-5">
@@ -101,25 +143,42 @@ export default function Main() {
           </p>
         </div>
       </div>
-      <a href="/" className="lg:px-0 px-4 z-10 hidden lg:block">
+      <a href="/" className="lg:px-0 px-4 z-10 hidden md:block">
         <img src="/images/Crypto.png" alt="Payments" className="rounded-2xl" />
       </a>
-      <div className="lg:grid grid-cols-2 gap-5 hidden">
-        <a href="/" className="lg:px-0 px-4 z-10">
-          <img
-            src="/images/Stocks.jpeg"
-            alt="Payments"
-            className="rounded-2xl"
-          />
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Crypto_Mobile.png"
+          alt="Crypto"
+          className="rounded-2xl"
+        />
+      </a>
+      <div className="md:grid grid-cols-2 gap-0 lg:gap-5 hidden">
+        <a href="/" className="lg:px-0 px-2 z-10">
+          <img src="/images/Stocks.jpeg" alt="Stocks" className="rounded-2xl" />
         </a>
-        <a href="/" className="lg:px-0 px-4 z-10">
+        <a href="/" className="lg:px-0 px-2 z-10">
           <img
             src="/images/Commodities.png"
-            alt="Payments"
+            alt="Commodities"
             className="rounded-2xl"
           />
         </a>
       </div>
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Stocks_Mobile.jpeg"
+          alt="Stocks"
+          className="rounded-2xl"
+        />
+      </a>
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Commodities_Mobile.png"
+          alt="Commodities"
+          className="rounded-2xl"
+        />
+      </a>
       <div className="mx-auto mt-20 lg:mt-44 mb-6 lg:mb-20 z-10 px-10 lg:px-0">
         <div className="text-center">
           <p className="Basier-Regular text-md lg:text-xl font-bold text-[#77828c] mb-5">
@@ -134,25 +193,42 @@ export default function Main() {
           </p>
         </div>
       </div>
-      <a href="/" className="lg:px-0 px-4 z-10 hidden lg:block">
+      <a href="/" className="lg:px-0 px-4 z-10 hidden md:block">
         <img src="/images/Stays.png" alt="Payments" className="rounded-2xl" />
       </a>
-      <div className="lg:grid grid-cols-2 gap-5 hidden">
-        <a href="/" className="lg:px-0 px-4 z-10">
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Stays_Mobile.png"
+          alt="Stays"
+          className="rounded-2xl"
+        />
+      </a>
+      <div className="md:grid grid-cols-2 gap-0 lg:gap-5 hidden">
+        <a href="/" className="lg:px-0 px-2 z-10">
           <img
             src="/images/Currencies_Small.jpeg"
-            alt="Payments"
+            alt="Currencies"
             className="rounded-2xl"
           />
         </a>
-        <a href="/" className="lg:px-0 px-4 z-10">
-          <img
-            src="/images/Lounge.png"
-            alt="Payments"
-            className="rounded-2xl"
-          />
+        <a href="/" className="lg:px-0 px-2 z-10">
+          <img src="/images/Lounge.png" alt="Lounge" className="rounded-2xl" />
         </a>
       </div>
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Currencies_Small_Mobile.jpeg"
+          alt="Currencies"
+          className="rounded-2xl"
+        />
+      </a>
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Lounge_Mobile.png"
+          alt="Lounge"
+          className="rounded-2xl"
+        />
+      </a>
       <div className="mx-auto mt-20 lg:mt-44 mb-6 lg:mb-20 z-10 px-10 lg:px-0">
         <div className="text-center">
           <p className="Basier-Regular text-md lg:text-xl font-bold text-[#77828c] mb-5">
@@ -168,18 +244,32 @@ export default function Main() {
           </p>
         </div>
       </div>
-      <div className="lg:grid grid-cols-2 gap-5 hidden">
-        <a href="/" className="lg:px-0 px-4 z-10">
-          <img src="/images/Gifts.png" alt="Payments" className="rounded-2xl" />
+      <div className="md:grid grid-cols-2 gap-0 lg:gap-5 hidden">
+        <a href="/" className="lg:px-0 px-2 z-10">
+          <img src="/images/Gifts.png" alt="Gifts" className="rounded-2xl" />
         </a>
-        <a href="/" className="lg:px-0 px-4 z-10">
+        <a href="/" className="lg:px-0 px-2 z-10">
           <img
             src="/images/Donations.png"
-            alt="Payments"
+            alt="Donations"
             className="rounded-2xl"
           />
         </a>
       </div>
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Gifts_Mobile.png"
+          alt="Gifts"
+          className="rounded-2xl"
+        />
+      </a>
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Donations_Mobile.png"
+          alt="Donations"
+          className="rounded-2xl"
+        />
+      </a>
       <div className="mx-auto mt-20 lg:mt-44 mb-6 lg:mb-20 z-10 px-10 lg:px-0">
         <div className="text-center">
           <p className="Basier-Regular text-md lg:text-xl font-bold text-[#77828c] mb-5">
@@ -192,6 +282,33 @@ export default function Main() {
             Get peace of mind with our strong security systems and trusted
             partners
           </p>
+        </div>
+      </div>
+      <a href="/" className="lg:px-0 px-4 z-10 hidden md:block">
+        <img
+          src="/images/Security.png"
+          alt="Security"
+          className="rounded-2xl"
+        />
+      </a>
+      <a href="/" className="lg:px-0 px-4 z-10 md:hidden block">
+        <img
+          src="/images/Security_Mobile.png"
+          alt="Security"
+          className="rounded-2xl"
+        />
+      </a>
+      <div className="mx-auto mt-20 lg:mt-44 mb-6 lg:mb-20 z-10 px-10 lg:px-0">
+        <div className="text-center">
+          <p className="Basier-SemiBold text-4xl lg:text-6xl font-bold mb-8">
+            Join 18M people who already trust us with their money
+          </p>
+          <a
+            className="inline-flex items-center py-[6px] px-4 font-medium text-white rounded-[10px] bg-[#191c1f]"
+            href="/"
+          >
+            <span className="justify-center">Get started</span>
+          </a>
         </div>
       </div>
     </main>
