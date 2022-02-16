c05a9c6322a80de6777101a7d6cc30919091d894
Jack Latimer <jacklatimer03@gmail.com>
2022-02-16 03:55:59 +0000

    do(footer): last footer section

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <>
-      <footer className="bg mt-20">
+      <footer className="mx-auto mt-20 grid w-full bg-[#191c1f]">
         <div className="mx-auto flex max-w-[1000px] flex-col gap-5">
           <div className="z-10 mx-auto mt-32 mb-6 px-4 lg:px-0">
             <div className="text-center">
@@ -12,8 +12,11 @@ export default function Footer() {
               </p>
             </div>
           </div>
-          <div className="mx-6 mb-0 grid grid-cols-1 space-y-6 space-x-0 text-[#191c1f] md:grid-cols-4 md:space-x-6 md:space-y-0 lg:mx-0 lg:space-x-6">
-            <div className="rounded-3xl bg-white pt-8 pb-6 pl-4 pr-2 lg:pt-8 lg:pb-8 lg:pl-6 lg:pr-9">
+          <div className="mx-6 mb-0 grid grid-cols-1 space-y-6 space-x-0 text-[#191c1f] md:grid-cols-4 md:space-x-6 md:space-y-0 lg:mx-0 lg:space-x-8">
+            <a
+              href="/"
+              className="relative rounded-3xl bg-white pt-8 pb-6 pl-4 pr-2 lg:pt-6 lg:pb-8 lg:pl-6 lg:pr-9"
+            >
               <span className="Basier-SemiBold text-[24px] lg:text-[34px]">
                 Standard
               </span>
@@ -25,8 +28,21 @@ export default function Footer() {
                 to your budget with our built-in budgeting, get more from your
                 money with our Standard account
               </span>
-            </div>
-            <div className="rounded-3xl bg-white pt-8 pb-6 pl-4 pr-2 lg:pt-8 lg:pb-8 lg:pl-6 lg:pr-9">
+              <div className="absolute bottom-0 right-0 mb-4 mr-5 text-[#75808a]">
+                <svg viewBox="0 0 24 24" size="24" className="h-6 w-6">
+                  <g fill="currentColor">
+                    <path
+                      fill="currentColor"
+                      d="M20.5 11.992c0-.276-.105-.535-.306-.737L14.77 5.832a.919.919 0 0 0-1.3 0l-.324.325a.919.919 0 0 0 0 1.3l3.394 3.395H4.42a.919.919 0 0 0-.92.919v.46c0 .507.411.918.92.918h12.12l-3.394 3.394a.919.919 0 0 0 0 1.3l.325.325c.359.359.94.359 1.3 0l5.423-5.422c.204-.205.306-.477.306-.754Z"
+                    ></path>
+                  </g>
+                </svg>
+              </div>
+            </a>
+            <a
+              href="/"
+              className="relative rounded-3xl bg-white pt-8 pb-6 pl-4 pr-2 lg:pt-6 lg:pb-8 lg:pl-6 lg:pr-9"
+            >
               <h2 className="Basier-SemiBold text-[24px] lg:text-[34px]">
                 Plus
               </h2>
@@ -37,8 +53,21 @@ export default function Footer() {
                 Give your everyday finances a boost. Get more out of your money
                 for less than the cost of a coffee
               </span>
-            </div>
-            <div className="rounded-3xl bg-white pt-8 pb-6 pl-4 pr-2 lg:pt-8 lg:pb-8 lg:pl-6 lg:pr-9">
+              <div className="absolute bottom-0 right-0 mb-4 mr-5 text-[#75808a]">
+                <svg viewBox="0 0 24 24" size="24" className="h-6 w-6">
+                  <g fill="currentColor">
+                    <path
+                      fill="currentColor"
+                      d="M20.5 11.992c0-.276-.105-.535-.306-.737L14.77 5.832a.919.919 0 0 0-1.3 0l-.324.325a.919.919 0 0 0 0 1.3l3.394 3.395H4.42a.919.919 0 0 0-.92.919v.46c0 .507.411.918.92.918h12.12l-3.394 3.394a.919.919 0 0 0 0 1.3l.325.325c.359.359.94.359 1.3 0l5.423-5.422c.204-.205.306-.477.306-.754Z"
+                    ></path>
+                  </g>
+                </svg>
+              </div>
+            </a>
+            <a
+              href="/"
+              className="relative rounded-3xl bg-white pt-8 pb-6 pl-4 pr-2 lg:pt-6 lg:pb-8 lg:pl-6 lg:pr-9"
+            >
               <h2 className="Basier-SemiBold text-[24px] lg:text-[34px]">
                 Premium
               </h2>
@@ -49,8 +78,21 @@ export default function Footer() {
                 Upgrade to a global lifestyle. Get the confidence to spend,
                 invest, and save smarter across the globe
               </span>
-            </div>
-            <div className="rounded-3xl bg-white pt-8 pb-6 pl-4 pr-2 lg:pt-8 lg:pb-8 lg:pl-6 lg:pr-9">
+              <div className="absolute bottom-0 right-0 mb-4 mr-5 text-[#75808a]">
+                <svg viewBox="0 0 24 24" size="24" className="h-6 w-6">
+                  <g fill="currentColor">
+                    <path
+                      fill="currentColor"
+                      d="M20.5 11.992c0-.276-.105-.535-.306-.737L14.77 5.832a.919.919 0 0 0-1.3 0l-.324.325a.919.919 0 0 0 0 1.3l3.394 3.395H4.42a.919.919 0 0 0-.92.919v.46c0 .507.411.918.92.918h12.12l-3.394 3.394a.919.919 0 0 0 0 1.3l.325.325c.359.359.94.359 1.3 0l5.423-5.422c.204-.205.306-.477.306-.754Z"
+                    ></path>
+                  </g>
+                </svg>
+              </div>
+            </a>
+            <a
+              href="/"
+              className="relative rounded-3xl bg-white pt-8 pb-6 pl-4 pr-2 lg:pt-6 lg:pb-8 lg:pl-6 lg:pr-9"
+            >
               <h2 className="Basier-SemiBold text-[24px] lg:text-[34px]">
                 Metal
               </h2>
@@ -62,7 +104,17 @@ export default function Footer() {
                 0.7% interest paid daily on savings, cashback, low-fee crypto
                 and much more
               </span>
-            </div>
+              <div className="absolute bottom-0 right-0 mb-4 mr-5 text-[#75808a]">
+                <svg viewBox="0 0 24 24" size="24" className="h-6 w-6">
+                  <g fill="currentColor">
+                    <path
+                      fill="currentColor"
+                      d="M20.5 11.992c0-.276-.105-.535-.306-.737L14.77 5.832a.919.919 0 0 0-1.3 0l-.324.325a.919.919 0 0 0 0 1.3l3.394 3.395H4.42a.919.919 0 0 0-.92.919v.46c0 .507.411.918.92.918h12.12l-3.394 3.394a.919.919 0 0 0 0 1.3l.325.325c.359.359.94.359 1.3 0l5.423-5.422c.204-.205.306-.477.306-.754Z"
+                    ></path>
+                  </g>
+                </svg>
+              </div>
+            </a>
           </div>
           <div className="z-10 mx-auto mt-6 mb-6 px-8 lg:mt-16 lg:mb-20 lg:px-0">
             <div className="text-center">
